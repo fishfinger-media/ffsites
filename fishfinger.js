2890697fb7fd71debecba2077c3d60b4a92edcce
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2025-01-31 12:22:59 +0000

    Update fishfinger.js

--- fishfinger.js
+++ fishfinger.js
@@ -1,11 +1,5 @@
 // FISHFINGER MEDIA BASE WEBFLOW JS
 
-document.addEventListener('DOMContentLoaded', function() {
-    var ffcomment = document.createComment("Made by Fishfinger Media - https://fishfinger.me");
-    var ffdoctype = document.doctype;
-    ffdoctype.parentNode.insertBefore(ffcomment, ffdoctype.nextSibling);
-});
-
 // COPYRIGHT DATE 
 const ffcurrentYear = new Date().getFullYear();
 document.querySelectorAll('[data-copyright]').forEach(element => element.innerHTML = ffcurrentYear);
